df2a04cb071246bed387b4b04d819967115755bc
Mpandey04 <107322513+Mpandey04@users.noreply.github.com>
2025-12-03 23:12:44 +0530

    Variables In javaScript

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,30 @@
+const mobNo=7258728
+let names="Manish Pandey"
+var Email="manishkumar831@gmail.com"
+console.log(mobNo);
+console.log(names);
+console.log(Email);
+// mobNo="85658" it's can't be executed becuase of const keyword.
+console.log(mobNo);
+
+Email="Pandeyanish@gmail.com"
+name="anish"
+console.log(Email);
+console.log(names);
+city="Delhi"
+
+let accountState;
+// We can use console.table([]) in palce of console.log
+
+console.table([mobNo,names,Email,city,accountState])
+
+/*
+We can create varibale in javascriptnin three ways(keyword)
+        1.let
+        2.var
+        3.const:-it is basically for constant values once we create any varible with const keyword it can't be change.
+
+ 1.let variable-
+ 2.Var:-prefer not to use var because of issue in block level scope and functional scope.
+ 
+*/
