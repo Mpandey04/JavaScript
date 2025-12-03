17f671ccc3335bf0a765699fd74177cd5bc5e4b6
Your Name <you@example.com>
2025-12-03 22:07:27 +0530

    DataTypes

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all Js code as newer version.
+
+// alert("Hello") /* alert can't be use in node js but we can use in direct console through any website becuase javascript ka engine ek document ke andar hai(browser ke andar hai)*/
+
+/*
+    DataTypes:
+    Premitive DataTypes:-following are basic Premitive Datatypes.
+    1.Number=> 2 to power 53
+    2.bigint
+    3.string=>anything between single queote('') or double queote ("")
+    4.boolean=>true or false 
+    5.null=> standalone value
+    6.Undefined=>
+    7.symbol=>mostly use for Uniquness
+
+    Object
+
+*/
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
+console.log(typeof(NaN));//number
+console.log(typeof(Infinity));//number
+console.log(typeof(Infinity)==Number);//false
+console.log(typeof(NaN)==Number);//false
+
+
+
+
+
+
+
+
+ 
