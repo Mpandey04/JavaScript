653ac5ff6ea82603821f469c7ab4ec83f3367e83
Your Name <you@example.com>
2025-12-06 21:54:47 +0530

    Memory Allocation

---
+++ 01_basic/06_Memory.js
@@ -0,0 +1,18 @@
+// Stack (Primitive),Heap(Non-primitive)
+
+let MyName="Manish kumar Pandey"
+let name=MyName
+
+name="RAaz"
+// console.log(MyName);
+// console.log(name);
+
+let UserOne={
+    useremail:"roh123@gmail.com",
+    upi:"roh432@ybl"
+}
+let UserTwo=UserOne
+
+console.log(UserTwo.upi);
+UserTwo.upi="roh8752@ybl"
+console.log(UserTwo.upi);
