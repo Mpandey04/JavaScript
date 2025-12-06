3406deb9be001a3ee485237c4e906e394a40947a
Mpandey04 <107322513+Mpandey04@users.noreply.github.com>
2025-12-06 19:36:20 +0530

    Comparision

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+//This type of conversion should be avoided.
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// strict check ===
+console.log("2"===2 );
