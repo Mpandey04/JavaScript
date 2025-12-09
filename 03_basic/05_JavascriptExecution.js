aaf811dde193a1146248ca225d13ff206fae1ae6
Your Name <you@example.com>
2025-12-09 21:14:05 +0530

    JavaScript Execution

---
+++ 03_basic/05_JavascriptExecution.js
@@ -0,0 +1,13 @@
+/*
+
+    Javascript Execution context.
+
+    step-1:-Global Execution context
+    step-2:-Function Execution context
+    step-3:- Eval Execution context
+
+
+    [{}]-> Memory creation phase
+        -> Execution phase
+
+*/
